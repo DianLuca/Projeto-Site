a963f6745569f9bc73bc922279c41feb5aecedfe
Dian Luca <dianlucca@hotmail.com>
2023-03-15 12:27:30 -0300

    first commit

---
+++ tec.js
@@ -0,0 +1,68 @@
+let tecJson = [
+    {
+        id: 1,
+        name: 'Big Data E Analytics',
+        img:'Tec/tecno1.jpg',
+        description:'Com grande capacidade de armazenar dados, podemos dizer que essa ferramenta possibilitou coletar, organizar e analisar um número de informações extremamente densas, de forma rápida. Auxiliando na otimização da produção, redução de custos, melhorando o desempenho dos equipamentos. Contribui também para uma tomada de decisão cada vez mais precisa sobre uma determinada questão que pode vir a acontecer, possibilitando um aumento da competitividade pela organização.'
+    }, {
+        id: 2,
+        name: 'Impressoras 3D',
+        img:'Tec/tecno2.jpg',
+        description:'Também conhecida como manufatura aditiva, a impressão 3D auxilia na produção de diversos produtos de forma personalizada. Esta tecnologia revolucionou a forma com que podemos produzir peças e outros produtos, seguindo especificações extremamente precisas. A otimização da utilização de matérias primas, como por exemplo o plástico, faz com que a produção utilize apenas o necessário para a confecção de cada peça. Atualmente somos capazes de até mesmo produzir alimentos com essa tecnologia.'
+    }, {
+        id: 3,
+        name: 'Nuvens Para Armazenamento e Processamento',
+        img:'Tec/tecno3.jpg',
+        description:'Utilizadas não só para armazenagem de dados, elas também podem ser utilizadas para o processamento em alta velocidade, assim como uma infinidade de outras atividades. Esses serviços podem ser acessados através de terminais, até mesmo de média performance, conectados à internet em qualquer lugar ou horário, e não ocupam espaço de armazenamento no seu dispositivo. A agilidade e praticidade da nuvem permite com que as organizações troquem informações de forma rápida para a tomada de decisão, sem a necessidade de ter um computador com alta capacidade de processamento em suas instalações, o que acarreta em redução os custos.   '
+    },{
+        id: 4,
+        name: 'Robôs',
+        img:'Tec/tecno4.jpg',
+        description:'Utilizados há algum tempo na indústria, os robôs possibilitam a redução do custo de mão-de-obra, por exemplo. Além disso, a troca da força de trabalho por robôs pode trazer mais segurança aos profissionais que antes precisariam estar em locais de risco. De forma autônoma, inteligente e cooperativa, com necessidade de pouca intervenção humana pois, uma vez programados, eles seguem os padrões predefinidos. Isso contribui para o aumento da produção e competitividade. Sabemos que abaixo de determinada profundidade, mesmo com todos os equipamentos necessários, somos incapazes de chegar. Portanto, usamos os robôs para a exploração de petróleo em alta profundidade como no pré-sal, por exemplo. Conhecidos como Veículo de Operação Remota, ou ROV, esse tipo de robô, permite que se chegue a profundidades muito altas no fundo do mar. '
+    },{
+        id: 5,
+        name: 'Drones',
+        img:'Tec/tecno5.jpg',
+        description:'Drones ou veículos aéreos não tripulados, podem ser utilizados em diversos seguimentos permitindo monitorar áreas extremamente grandes, densas e até ambientes hostis. Utilizados na agropecuária, por exemplo, tornou o monitoramento de plantações mais fácil, permitindo o controle da produção e o acompanhamento da evolução do plantio de forma remota, sem a necessidade de deslocamento até o local. Outras áreas, como a audiovisual, utilizam do drones para a captura de imagem de uma forma que normalmente não conseguiríamos, com alta qualidade de resolução. Drones também são utilizados pelos militares para monitoramento, auxílio e controle de regiões onde não seria possível mandar tropas.  '
+    },{
+        id: 6,
+        name: 'Home Broker',
+        img:'Tec/tecno6.jpg',
+        description:'Trata-se de um sistema utilizado principalmente na bolsa de valores que permite o investimento e acompanhamento de ações negociadas em tempo real e remotamente, por meio da internet. Podendo ser utilizado em qualquer plataforma, computador, celular e outros, permite a negociação e tomada de decisão pelo usuário, de forma rápida, para a compra e venda no mercado de ações. O Home Broker é utilizado por diversas corretoras. Portanto vale lembrar que cada uma tem um foco específico para a utilização dessa ferramenta, entretanto contam com funcionalidades comuns.'
+    },{
+        id: 7,
+        name: 'Trading Algorítmico',
+        img:'Tec/tecno7.jpg',
+        description:'Trata-se de uma ferramenta utilizada no meio comercial para analisar o que acontecerá com uma ação, por exemplo. O Trading Algorítmico avalia os dados com base nas cotações atuais e históricas de um ativo, a fim de apresentar as possíveis variações que a ação sofrerá no mercado. Entretanto, somada a uma análise precisa por parte de uma pessoa capacitada para essa situação, favorece o posicionamento perante o resto do mercado. Podemos citar nesse meio a utilização de robôs para negociação para as criptomoedas, como é o exemplo de: Bitcoin Evolution, Bitcoin Trader, que são traders que auxiliam seus usuários a tomar decisões sobre compra e venda das suas criptomoedas.'
+    },{
+        id: 8,
+        name: 'Veículos Autônomos',
+        img:'Tec/tecno8.jpg',
+        description:'Por terem um grande potencial para melhorar a mobilidade das grandes cidades, os veículos autônomos se movimentam conforme programado para chegar a um determinado destino de forma rápida, segura e eficiente. Com o uso de sensores e câmeras para auxiliar seu percurso os AV’s tornam dispensáveis a necessidade de um motorista, o que pode trazer comodidade e segurança ao usuário.'
+    },{
+        id: 9,
+        name: 'Data Mining',
+        img:'Tec/tecno9.jpg',
+        description:'A mineração de dados, em português, trata da capacidade de obter informações uteis dentro de uma grande rede de dados que são deixados em sites e aplicativos. Ou seja, examinar as informações contidas na Big Data, para que seja possível conhecer melhor o público a ser atendido, de acordo com o seu padrão de consumo. Sabemos que fazer esse tipo de análise atualmente é humanamente impossível, e essa tecnologia veio para auxiliar a identificar as possíveis tendências, entender o padrão de consumo e saber o que motiva o cliente a realizar uma compra. “Data Mining resgata em organizações grandes o papel do dono atendendo no balcão e conhecendo sua clientela” (CETAX, 2016). '
+    },{
+        id: 10,
+        name: 'Business Inteligence (BI)',
+        img:'Tec/tecno10.jpg',
+        description:'BI é uma ferramenta que visa dar suporte à tomada de decisão, conforme os dados já obtidos em sua base de dados. BI é uma ferramenta onde vemos a necessidade da presença humana, pois exige um conhecimento necessário para a tomada de decisão correta por meio da avaliação dos dados coletados. Prevenindo riscos e possibilitando a identificação de oportunidades de forma que seja essencial para o posicionamento da empresa diante ao mercado. Podemos citar como exemplo o Power BI da Microsoft, uma ferramenta que auxilia na análise de todos esses dados.'
+    },{
+        id: 11,
+        name: 'Machine Learning',
+        img:'Tec/tecno11.jpg',
+        description:'Machine Learning ou aprendizado das máquinas, acontece a partir do momento em que as máquinas começam a receber informações para realizar atividades de forma autônoma. Ou seja, conforme recebem informações elas aplicam e atualizam o processo de forma natural, sendo capaz de analisar e reconhecer padrões para tornar a sua atuação mais efetiva. Podemos citar como exemplo para essa tecnologia os bancos de dados autônomos, tradutores de texto, serviços de combate a fraudes, entre outras, que devido ao seu grande número de acessos e grande base de dados torna possível a aprendizagem dessas ferramentas de forma rápida, atendendo às expectativas das organizações. '
+    },{
+        id: 12,
+        name: 'Deep Learning',
+        img:'Tec/tecno12.jpg',
+        description:'Conhecida também como aprendizagem profunda é uma das tecnologias ligadas ao Machine Learning. Utilizando de camadas inspiradas nas redes neurais humanas, essa ferramenta tornou possível analisar uma gama maior e mais complexa de dados, como reconhecimento por voz, identificação de imagens, entre outros tipos e formatos de dados. Cada vez mais essa tecnologia vem se aperfeiçoando, sendo capaz de fazer reconhecimento facial, detectar objetos, entre outros, sem a necessidade de qualquer interferência humana.'
+    },{
+        id: 13,
+        name: 'Smart Manufacturing',
+        img:'Tec/tecno13.jpg',
+        description:'A utilização da internet das coisas (IoT) para monitorar os processos produtivos, identificando oportunidades e analisando dados para melhorar a performance e ações a serem tomadas. Sendo capaz de prever quando uma máquina precisará de manutenção, por exemplo. Isso previne que não aconteça uma parada na produção, de forma desnecessária, o que pode ocasionar perdas para a organização. Vale lembrar que essa tecnologia também é capaz de controlar o estoque, avaliando quando existe a necessidade de reposição e redirecionamento da produção. Sendo assim, são capazes de atender às necessidades dos clientes de forma rápida e eficiente.'
+    }
+];
